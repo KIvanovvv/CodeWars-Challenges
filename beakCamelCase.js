59ffc0da81ea623a6fe1f6d920dcdadb439a3d20
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-09-11 17:28:05 +0100

    Refactor challenge

--- beakCamelCase.js
+++ beakCamelCase.js
@@ -1,19 +1,5 @@
 function solution(string) {
-  let indexArr = [];
-  let startIndex = 0;
-  let resultArr = [];
-  for (let i = 0; i < string.length; i++) {
-    if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
-      indexArr.push(i);
-    }
-  }
-  indexArr.forEach((el) => {
-    let currWord = string.slice(startIndex, el);
-    startIndex = el;
-    resultArr.push(currWord);
-  });
-  resultArr.push(string.slice(startIndex));
-  return resultArr.join(" ");
+  return string.replace(/([A-Z])/g, ` $1`);
 }
 console.log(solution("camelArwDAws"));
 
